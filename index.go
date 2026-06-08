7a9f5191aac7a9018b0d430044c919d8c14b164d
alvin tan <alvintanuwijaya12@gmail.com>
2026-06-09 01:11:21 +0700

    Update index.go

--- index.go
+++ index.go
@@ -30,7 +30,7 @@ type data struct {
 
 type tabData [NMAX]data
 
-func tampilanAdmin() {
+func tampilanAdmin(pilihan int) {
 	fmt.Println("=== MENU ADMIN ===")
 	fmt.Println("1. Tambah Data")
 	fmt.Println("2. Hapus Data")
@@ -38,7 +38,6 @@ func tampilanAdmin() {
 	fmt.Println("4. Ubah Data Pemilik")
 	fmt.Println("5. Tambah Riwayat Servis")
 	fmt.Println("6. Tampilkan Data")
-	fmt.Println("0. Kembali")
 }
 
 func tampilanManager() {
@@ -48,30 +47,28 @@ func tampilanManager() {
 	fmt.Println("3. Selection Sort Tahun")
 	fmt.Println("4. Insertion Sort Tanggal")
 	fmt.Println("5. Statistik")
-	fmt.Println("0. Kembali")
 }
 
 func tambahData(k *tabData, nData *int) {
 	var tambah string
-
 	tambah = "y"
-
 	for *nData < NMAX && tambah == "y" {
-		fmt.Scan(
-			&(*k)[*nData].pemilik.nama,
-			&(*k)[*nData].pemilik.alamat,
-			&(*k)[*nData].pemilik.nomorTelepon,
-		)
-
-		fmt.Scan(
-			&(*k)[*nData].kendaraan.platNomor,
-			&(*k)[*nData].kendaraan.tahunProduksi,
-		)
-
-		*nData = *nData + 1
-		fmt.Scan(&tambah)
+			fmt.Scan(
+				&(*k)[*nData].pemilik.nama,
+				&(*k)[*nData].pemilik.alamat,
+				&(*k)[*nData].pemilik.nomorTelepon,
+			)
+	
+			fmt.Scan(
+				&(*k)[*nData].kendaraan.platNomor,
+				&(*k)[*nData].kendaraan.tahunProduksi,
+			)
+	
+			*nData = *nData + 1
+			fmt.Scan(&tambah)
+		}
 	}
-}
+
 
 func hapusDataKendaraan(k *tabData, nData *int) {
 	var index int
@@ -81,9 +78,10 @@ func hapusDataKendaraan(k *tabData, nData *int) {
 	if index <= 0 || index >= *nData {
 		fmt.Println("Data tidak ditemukan!")
 	} else {
+
 		for index <= *nData-2 {
 			(*k)[index] = (*k)[index+1]
-			index = index + 1
+			index++
 		}
 
 		*nData = *nData - 1
@@ -100,6 +98,7 @@ func ubahDataKendaraan(k *tabData, nData int) {
 	if index <= 0 || index >= nData {
 		fmt.Println("Index tidak ditemukan!")
 	} else {
+
 		fmt.Println(
 			"Data lama :",
 			(*k)[index].kendaraan.platNomor,
@@ -122,6 +121,7 @@ func ubahDataPemilik(k *tabData, nData int) {
 	if index <= 0 || index >= nData {
 		fmt.Println("Index tidak ditemukan!")
 	} else {
+
 		fmt.Println(
 			"Data lama :",
 			(*k)[index].pemilik.nama,
@@ -143,8 +143,11 @@ func tambahDataRiwayat(k *tabData, nData int) {
 	fmt.Scan(&index)
 
 	if index <= 0 || index >= nData {
+
 		fmt.Println("Index tidak ditemukan!")
+
 	} else {
+
 		fmt.Scan(
 			&(*k)[index].riwayatServis.jenisKerusakan,
 			&(*k)[index].riwayatServis.tanggalPerbaikan,
@@ -162,7 +165,9 @@ func sequence(k tabData, nData int) {
 	found = false
 
 	for index = 1; index < nData; index++ {
+
 		if platNomor == k[index].kendaraan.platNomor {
+
 			fmt.Println(
 				k[index].kendaraan.platNomor,
 				k[index].kendaraan.tahunProduksi,
@@ -172,7 +177,7 @@ func sequence(k tabData, nData int) {
 		}
 	}
 
-	if found == false {
+	if !found {
 		fmt.Println("Data tidak ditemukan!")
 	}
 }
@@ -184,24 +189,26 @@ func urutanData(k *tabData, nData int) {
 	pass = 2
 
 	for pass < nData {
+
 		idx = pass - 1
 		i = pass
 
 		for i < nData {
+
 			if (*k)[i].kendaraan.platNomor <
 				(*k)[idx].kendaraan.platNomor {
 
 				idx = i
 			}
 
-			i = i + 1
+			i++
 		}
 
 		temp = (*k)[pass-1]
 		(*k)[pass-1] = (*k)[idx]
 		(*k)[idx] = temp
 
-		pass = pass + 1
+		pass++
 	}
 }
 
@@ -219,11 +226,6 @@ func tampilan(k tabData, nData int) {
 			k[i].kendaraan.platNomor,
 			k[i].kendaraan.tahunProduksi,
 		)
-
-		fmt.Println(
-			k[i].riwayatServis.jenisKerusakan,
-			k[i].riwayatServis.tanggalPerbaikan,
-		)
 	}
 }
 
@@ -231,7 +233,6 @@ func binary(k tabData, nData int) {
 	var left, right, mid int
 	var platNomor string
 	var found bool
-
 	urutanData(&k, nData)
 
 	fmt.Scan(&platNomor)
@@ -239,11 +240,15 @@ func binary(k tabData, nData int) {
 	left = 1
 	right = nData - 1
 	found = false
+	
+	
 
 	for left <= right && found == false {
+
 		mid = (left + right) / 2
 
 		if platNomor == k[mid].kendaraan.platNomor {
+
 			fmt.Println(
 				k[mid].kendaraan.platNomor,
 				k[mid].kendaraan.tahunProduksi,
@@ -251,10 +256,13 @@ func binary(k tabData, nData int) {
 
 			found = true
 
+			left = right + 1
+
 		} else if platNomor < k[mid].kendaraan.platNomor {
 			right = mid - 1
 
 		} else {
+
 			left = mid + 1
 		}
 	}
@@ -271,24 +279,26 @@ func tahunSelection(k *tabData, nData int) {
 	pass = 1
 
 	for pass < nData-1 {
+
 		idx = pass
 		i = pass + 1
 
 		for i < nData {
+
 			if (*k)[i].kendaraan.tahunProduksi <
 				(*k)[idx].kendaraan.tahunProduksi {
 
 				idx = i
 			}
 
-			i = i + 1
+			i++
 		}
 
 		temp = (*k)[pass]
 		(*k)[pass] = (*k)[idx]
 		(*k)[idx] = temp
 
-		pass = pass + 1
+		pass++
 	}
 }
 
@@ -299,6 +309,7 @@ func tanggalInsertion(k *tabData, nData int) {
 	pass = 2
 
 	for pass < nData {
+
 		temp = (*k)[pass]
 		i = pass - 1
 
@@ -307,12 +318,12 @@ func tanggalInsertion(k *tabData, nData int) {
 				(*k)[i].riwayatServis.tanggalPerbaikan {
 
 			(*k)[i+1] = (*k)[i]
-			i = i - 1
+			i--
 		}
 
 		(*k)[i+1] = temp
 
-		pass = pass + 1
+		pass++
 	}
 }
 
@@ -322,6 +333,7 @@ func tampilkanStatistik(k tabData, nData int) {
 	fmt.Println("Jumlah data :", nData-1)
 
 	for i = 1; i < nData; i++ {
+
 		fmt.Println(
 			k[i].kendaraan.platNomor,
 			k[i].riwayatServis.jenisKerusakan,
@@ -329,89 +341,20 @@ func tampilkanStatistik(k tabData, nData int) {
 	}
 }
 
-func menuAdmin(k *tabData, nData *int) {
-	var pilihan int
-
-	pilihan = -1
-
-	for pilihan != 0 {
-		tampilanAdmin()
-		fmt.Scan(&pilihan)
-
-		if pilihan == 1 {
-			tambahData(k, nData)
-
-		} else if pilihan == 2 {
-			hapusDataKendaraan(k, nData)
-
-		} else if pilihan == 3 {
-			ubahDataKendaraan(k, *nData)
-
-		} else if pilihan == 4 {
-			ubahDataPemilik(k, *nData)
-
-		} else if pilihan == 5 {
-			tambahDataRiwayat(k, *nData)
-
-		} else if pilihan == 6 {
-			tampilan(*k, *nData)
-		}
-	}
-}
-
-func menuManager(k *tabData, nData int) {
-	var pilihan int
-
-	pilihan = -1
-
-	for pilihan != 0 {
-		tampilanManager()
-		fmt.Scan(&pilihan)
-
-		if pilihan == 1 {
-			sequence(*k, nData)
-
-		} else if pilihan == 2 {
-			binary(*k, nData)
-
-		} else if pilihan == 3 {
-			tahunSelection(k, nData)
-			tampilan(*k, nData)
-
-		} else if pilihan == 4 {
-			tanggalInsertion(k, nData)
-			tampilan(*k, nData)
-
-		} else if pilihan == 5 {
-			tampilkanStatistik(*k, nData)
-		}
-	}
-}
-
 func main() {
-	var k tabData
-	var pilihan int
 
-	pilihan = -1
+	var k tabData
 
 	fmt.Println("====================================================")
 	fmt.Println("||                 SELAMAT DATANG                 ||")
 	fmt.Println("====================================================")
 	fmt.Println("")
 
-	for pilihan != 0 {
-		fmt.Println("=== MENU UTAMA ===")
-		fmt.Println("1. Admin")
-		fmt.Println("2. Manager")
-		fmt.Println("0. Keluar")
+	tambahData(&k, &nData)
 
-		fmt.Scan(&pilihan)
+	tampilan(k, nData)
 
-		if pilihan == 1 {
-			menuAdmin(&k, &nData)
+	urutanData(&k, nData)
 
-		} else if pilihan == 2 {
-			menuManager(&k, nData)
-		}
-	}
+	tampilan(k, nData)
 }
